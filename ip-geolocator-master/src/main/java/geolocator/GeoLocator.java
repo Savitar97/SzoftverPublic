e54e38400c495413dc163d23151e72274f88007c
Savitar97 <atoth1571@gmail.com>
2020-03-30 22:49:33 +0200

    Geolocation example with tinylog and slf4j

--- ip-geolocator-master/src/main/java/geolocator/GeoLocator.java
+++ ip-geolocator-master/src/main/java/geolocator/GeoLocator.java
@@ -9,6 +9,8 @@ import com.google.gson.Gson;
 import com.google.common.net.UrlEscapers;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class for obtaining geolocation information about an IP address or host
@@ -21,7 +23,7 @@ public class GeoLocator {
      * URI of the geolocation service.
      */
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
-
+    private static Logger logger = LoggerFactory.getLogger(GeoLocator.class);
     private static Gson GSON = new Gson();
 
     /**
@@ -52,20 +54,28 @@ public class GeoLocator {
         URL url;
         if (ipAddrOrHost != null) {
             ipAddrOrHost = UrlEscapers.urlPathSegmentEscaper().escape(ipAddrOrHost);
+            logger.info("You give this location:{}",ipAddrOrHost);
             url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+            logger.info("The program download data from:{}",url);
+
         } else {
+
             url = new URL(GEOLOCATOR_SERVICE_URI);
+            logger.info("Uses your current location! Your current location is {}",url);
         }
         String s = IOUtils.toString(url, "UTF-8");
         return GSON.fromJson(s, GeoLocation.class);
     }
 
     public static void main(String[] args) throws IOException {
+        logger.warn("If you don't enter an ip address, then the program will use your current location!");
         try {
             String arg = args.length > 0 ? args[0] : null;
             System.out.println(new GeoLocator().getGeoLocation(arg));
+            logger.debug("Debugging {}",new GeoLocator().getGeoLocation("www.google.hu"));
         } catch (IOException e) {
             System.err.println(e.getMessage());
+            logger.error("I/O error", e.getMessage());
         }
     }
 
